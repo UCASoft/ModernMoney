f0954054a19467fa2bcdfeb5de74606692b06304
Sergey <AntonovSergeyS@gmail.com>
2025-11-09 13:35:38 +0100

    Add Ktor

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -42,6 +42,11 @@ kotlin {
                 api(libs.settings)
 
                 implementation(libs.kotlinx.serialization.json)
+
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.cio)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.serialization.json)
             }
         }
 
