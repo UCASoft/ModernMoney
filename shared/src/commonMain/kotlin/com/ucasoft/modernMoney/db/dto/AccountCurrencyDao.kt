f7533227266abe5ad3fc6f1a93853261f78728ca
Sergey <AntonovSergeyS@gmail.com>
2025-11-30 18:46:29 +0100

    Rename method

--- shared/src/commonMain/kotlin/com/ucasoft/modernMoney/db/dto/AccountCurrencyDao.kt
+++ shared/src/commonMain/kotlin/com/ucasoft/modernMoney/db/dto/AccountCurrencyDao.kt
@@ -8,7 +8,7 @@ import com.ucasoft.modernMoney.db.model.AccountCurrency
 @Dao
 interface AccountCurrencyDao {
     @Query("SELECT * FROM account_currencies WHERE accountId = :accountId")
-    suspend fun accountCurrency(accountId: Long) : List<AccountCurrency>
+    suspend fun accountCurrencies(accountId: Long) : List<AccountCurrency>
 
     @Insert
     suspend fun insert(currency: AccountCurrency)
