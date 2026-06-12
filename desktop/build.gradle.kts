f5ac83cd55f3e749aa5f5cc89dcc0492f368a530
Sergey <AntonovSergeyS@gmail.com>
2026-06-12 15:49:52 +0200

    Add icons

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -10,11 +10,25 @@ kotlin {
     /*linuxX64()
     mingwX64()*/
     sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.compose.components.resources)
+            }
+        }
         val jvmMain by getting {
             dependencies {
                 implementation(project(":shared"))
                 implementation(compose.desktop.currentOs)
             }
         }
+    }
+}
+
+compose {
+    resources {
+        customDirectory(
+            "jvmMain",
+            provider { layout.projectDirectory.dir("src/jvmMain/resources") }
+        )
     }
 }
--- desktop/src/jvmMain/kotlin/com/ucasoft/modernMoney/Main.kt
+++ desktop/src/jvmMain/kotlin/com/ucasoft/modernMoney/Main.kt
@@ -5,14 +5,19 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.awaitApplication
 import androidx.compose.ui.window.rememberWindowState
 import com.ucasoft.modernMoney.workers.startCurrencyExchangeSync
+import com.ucasoft.modern_money.desktop.generated.resources.Res
+import com.ucasoft.modern_money.desktop.generated.resources.icon
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = runBlocking {
     startCurrencyExchangeSync()
     awaitApplication {
         Window(
             onCloseRequest = ::exitApplication,
-            state = rememberWindowState(width = 1024.dp, height = 768.dp)
+            state = rememberWindowState(width = 1024.dp, height = 768.dp),
+            title = "Modern Money",
+            icon = painterResource(Res.drawable.icon)
         ) {
             App()
         }
