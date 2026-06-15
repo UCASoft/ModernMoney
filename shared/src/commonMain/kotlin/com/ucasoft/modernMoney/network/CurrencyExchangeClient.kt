03ba94f2d32531e108c15c6c40892964d9d76f12
Sergey <AntonovSergeyS@gmail.com>
2026-06-15 18:59:39 +0200

    Fix Currency Exchange Client

--- shared/src/commonMain/kotlin/com/ucasoft/modernMoney/network/CurrencyExchangeClient.kt
+++ shared/src/commonMain/kotlin/com/ucasoft/modernMoney/network/CurrencyExchangeClient.kt
@@ -3,6 +3,7 @@ package com.ucasoft.modernMoney.network
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.double
 import org.koin.core.component.KoinComponent
@@ -13,11 +14,12 @@ class CurrencyExchangeClient: KoinComponent {
     private val client by inject<HttpClient>()
 
     suspend fun fetchExchangeRates(currencies: Pair<String, List<String>>): Map<String, Double> {
-        val response: Map<String, JsonPrimitive> = client.get(
+        val response: Map<String, JsonElement> = client.get(
             "https://freecurrencyrates.com/api/action.php?s=fcr&v=1&do=cvals&f=${currencies.first}&iso=${
                 currencies.second.joinToString("-")
             }"
         ).body()
-        return response.filterKeys { it != "updated" && it != "foreign" }.mapValues { it.value.double }
+        return response.filterKeys { it != "updated" && it != "foreign" }
+            .mapNotNull { (key, value) -> (value as? JsonPrimitive)?.double?.let { key to it } }.toMap()
     }
 }
