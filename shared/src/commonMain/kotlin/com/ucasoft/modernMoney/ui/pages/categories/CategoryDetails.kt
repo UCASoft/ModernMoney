26bf7cdebfbf006ab0ae979a80890db8d13de3d2
Sergey <AntonovSergeyS@gmail.com>
2026-08-06 23:37:42 +0200

    Extend Category Details UI

--- shared/src/commonMain/kotlin/com/ucasoft/modernMoney/ui/pages/categories/CategoryDetails.kt
+++ shared/src/commonMain/kotlin/com/ucasoft/modernMoney/ui/pages/categories/CategoryDetails.kt
@@ -1,14 +1,31 @@
 package com.ucasoft.modernMoney.ui.pages.categories
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.ucasoft.components.scrollable.ScrollableLazyVerticalGrid
 import com.ucasoft.modernMoney.model.Category
 import com.ucasoft.modernMoney.ui.pages.DetailsMode
 import com.ucasoft.modernMoney.ui.pages.LogoEntityDetails
 import com.ucasoft.modernMoney.viewModels.category.CategoryUiState
 import com.ucasoft.modernMoney.viewModels.category.CategoryViewModel
+import com.ucasoft.modern_money.shared.generated.resources.Res
+import com.ucasoft.modern_money.shared.generated.resources.allDrawableResources
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun CategoryDetails(id: Long?, mode: DetailsMode = DetailsMode.VIEW) {
@@ -21,6 +38,8 @@ fun CategoryDetails(id: Long?, mode: DetailsMode = DetailsMode.VIEW) {
             )
         },
         { categoryState, viewModel, _ ->
+
+            var showDialog by remember { mutableStateOf(false) }
 
             CategoryDropDown(categoryState.parentCategory) {
                 viewModel.updateParentCategory(it)
@@ -41,6 +60,66 @@ fun CategoryDetails(id: Long?, mode: DetailsMode = DetailsMode.VIEW) {
                 isError = categoryState.errors.contains("name"),
                 supportingText = { Text(categoryState.errors["name"] ?: "") }
             )
+
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(end = 8.dp),
+                horizontalArrangement = Arrangement.End,
+            ) {
+                Button(
+                    onClick = {
+                        showDialog = true
+                    }
+                ) {
+                    Text("Select Logo")
+                }
+            }
+
+            if (showDialog) {
+                var selectedLogoName by remember { mutableStateOf<String?>(null) }
+                AlertDialog(
+                    onDismissRequest = {
+                        showDialog = false
+                    },
+                    confirmButton = {
+                        TextButton(onClick = {
+                            if (selectedLogoName != null) {
+                                viewModel.updateLogo(selectedLogoName)
+                            }
+                            showDialog = false
+                        }) {
+                            Text("Ok")
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(onClick = {
+                            showDialog = false
+                        }) {
+                            Text("Cancel")
+                        }
+                    },
+                    text = {
+                        ScrollableLazyVerticalGrid(
+                            columns = GridCells.Adaptive(96.dp),
+                            contentPadding = PaddingValues(8.dp)
+                        ) {
+                            items(Res.allDrawableResources.toList().filter { it.first.startsWith("category_") }) {
+                                val isSelected = it.first == selectedLogoName
+                                Image(
+                                    painterResource(it.second),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(96.dp).selectable(isSelected) {
+                                        selectedLogoName = it.first
+                                    }.border(
+                                        width = if (isSelected) 3.dp else 0.dp,
+                                        color = if (isSelected) MaterialTheme.colorScheme.primary else Color.Transparent,
+                                        shape = RoundedCornerShape(4.dp)
+                                    )
+                                )
+                            }
+                        }
+                    }
+                )
+            }
         },
         { categoryState, viewModel ->
             when(mode) {
